d7c47cd91b60a0e2ad8ecd0d44306f0dee8d97a1
KRofLife <05410kira@gmail.com>
2021-09-08 12:38:27 +0000

    layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,9 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
