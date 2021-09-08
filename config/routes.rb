219fb916c64e23dcc17f6c9ee6ae156dfcd8b93d
KRofLife <05410kira@gmail.com>
2021-09-08 03:30:06 +0000

    addrelationships

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :plans, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update, :index]
+
+  resources :users, only: [:show, :edit, :update, :index] do
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
